d2c2856da08e52cbf8cb59df812ff1e74e979887
Max Electos <spb.max2@gmail.com>
2018-08-23 12:08:19 +0200

    Fix block desk hardness value and particles

--- src/main/java/fr/romax/mffjam/common/blocks/ModBlocks.java
+++ src/main/java/fr/romax/mffjam/common/blocks/ModBlocks.java
@@ -10,7 +10,7 @@ public class ModBlocks {
 
 	public static void registerBlocks() {
 		// A utiliser : BlocksRegistry.register(block, name);
-		BlocksRegistry.register(new BlockDesk(), ItemDeskBlock::new, "desk");
+		BlocksRegistry.register(new BlockDesk().setHardness(2.5F), ItemDeskBlock::new, "desk");
 		GameRegistry.registerTileEntity(TileEntityDeskMain.class, new ResourceLocation(MFFJam.MODID, "tile_desk_main"));
 		GameRegistry.registerTileEntity(TileEntityDeskSec.class, new ResourceLocation(MFFJam.MODID, "tile_desk_sec"));
 	}
