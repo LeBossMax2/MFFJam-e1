a857fe537c84cfba34c275308252a433484dc442
Romain120105 <romain120105@outlook.fr>
2018-08-24 14:56:04 +0200

    Adding base of messager villager

--- src/main/java/fr/romax/mffjam/common/entities/ModEntities.java
+++ src/main/java/fr/romax/mffjam/common/entities/ModEntities.java
@@ -1,8 +1,11 @@
 package fr.romax.mffjam.common.entities;
 
+import java.awt.Color;
+
 import fr.romax.mffjam.MFFJam;
 import fr.romax.mffjam.client.renderers.entity.RenderHangingMessage;
 import fr.romax.mffjam.client.renderers.entity.RenderMessageArrow;
+import fr.romax.mffjam.client.renderers.entity.RenderVillagerMessager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
@@ -18,6 +21,8 @@ public class ModEntities
 	{
 		EntityRegistry.registerModEntity(new ResourceLocation(MFFJam.MODID, "message_arrow"), EntityMessageArrow.class, "message_arrow", 0, MFFJam.instance, 64, 20, false);
 		EntityRegistry.registerModEntity(new ResourceLocation(MFFJam.MODID, "hanging_message"), EntityHangingMessage.class, "hanging_message", 1, MFFJam.instance, 160, Integer.MAX_VALUE, false);
+		EntityRegistry.registerModEntity(new ResourceLocation(MFFJam.MODID, "villager_messager"), EntityVillagerMessager.class, "villager_messager", 2, MFFJam.instance, 60, 20, false, Color.black.getRGB(), Color.white.getRGB());
+
 	}
 	
 	@SideOnly(Side.CLIENT)
@@ -25,6 +30,7 @@ public class ModEntities
 	{
 		RenderingRegistry.registerEntityRenderingHandler(EntityMessageArrow.class, RenderMessageArrow::new);
 		RenderingRegistry.registerEntityRenderingHandler(EntityHangingMessage.class, RenderHangingMessage<EntityHangingMessage>::new);
+		RenderingRegistry.registerEntityRenderingHandler(EntityVillagerMessager.class, RenderVillagerMessager::new);
 	}
 	
 }
